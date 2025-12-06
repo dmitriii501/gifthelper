05f3833a29c70d92d9e7f973131dcb7a6646215a
dmitriii501 <dimahydacov290483@gmail.com>
2025-12-06 22:17:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -142,5 +142,6 @@ async def main():
     print("Бот запущен...")
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+print("Бот запущен...")
+asyncio.run(main())
+
